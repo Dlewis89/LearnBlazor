d40e1768f40ee95bfd5a56d87748810dc0d98f3e
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-08-21 13:20:18 -0400

    Two way binding

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,6 +9,7 @@ namespace LearnBlazor.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public double Price { get; set; }
         public bool IsActive { get; set; }
     }
 }
